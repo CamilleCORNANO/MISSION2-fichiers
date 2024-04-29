8a33b357b392b46f38a8ed756c3e00b24ea84631
CamilleCORNANO <158443958+CamilleCORNANO@users.noreply.github.com>
2024-04-29 08:32:17 +0200

    Add files via upload

---
+++ site P2/controleur/controleurPrincipal.php
@@ -0,0 +1,19 @@
+<?php
+
+function controleurPrincipal($action) {
+    $lesActions = array();
+    $lesActions["defaut"] = "listeRestos.php";
+    $lesActions["liste"] = "listeRestos.php";
+    $lesActions["connexion"] = "connexion.php";
+    $lesActions["deconnexion"] = "deconnexion.php";
+    $lesActions["recherche"] = "rechercheResto.php";
+    $lesActions["resultat"] = "ResultRecherche.php";
+
+    if (array_key_exists($action, $lesActions)) {
+        return $lesActions[$action];
+    } else {
+        return $lesActions["defaut"];
+    }
+}
+
+?>
